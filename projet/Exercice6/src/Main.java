9bf9d8f9a34c86672cbfd1f380cf80ba014f3032
Tom <tomdesvignes031@gmail.com>
2023-07-11 15:52:35 +0200

    exercice 6

---
+++ projet/Exercice6/src/Main.java
@@ -0,0 +1,11 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    // La méthode main pour tester le bon fonctionnement de la classe Client
+    public static void main(String[] args) {
+        // Création d'un objet Client avec des valeurs initiales
+        Client client = new Client("Dupont", "Jean", "1 rue de la Paix", "75008", "Paris");
+        // Affichage de l'objet Client
+        System.out.println(client);
+    }
+}
