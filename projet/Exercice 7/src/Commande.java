6b3ec79a0f38f6a2c04949bf931029f7f868438d
Tom <tomdesvignes031@gmail.com>
2023-07-11 16:19:51 +0200

    exercice 7

--- projet/Exercice 7/src/Commande.java
+++ projet/Exercice 7/src/Commande.java
@@ -1,3 +1,52 @@
-public class Commande {
 
+import java.util.ArrayList;
+
+public class Commande {
+    // L'attribut de la classe Commande
+    private ArrayList<Article> articles;
+    private double total;
+
+    // Le constructeur de la classe Commande
+    public Commande() {
+        // Initialisation de l'attribut articles avec une liste vide
+        articles = new ArrayList<Article>();
+        // Initialisation de l'attribut total à 0
+        total = 0;
+    }
+
+    // La méthode pour ajouter un article à la commande
+    public void ajouterArticle(Article article) {
+        // Ajout de l'article à la liste des articles
+        articles.add(article);
+        // Mise à jour du total de la commande
+        total += article.getPrix();
+    }
+
+    // La méthode pour calculer le total de la commande
+    public double calculerTotal() {
+        // Initialisation du total à 0
+        double total = 0;
+        // Pour chaque article dans la liste des articles
+        for (Article article : articles) {
+            // Ajout du prix de l'article au total
+            total += article.getPrix();
+        }
+        // Retourne le total de la commande
+        return total;
+    }
+
+    // La méthode pour afficher les articles de la commande
+    public void afficherArticles() {
+        // Pour chaque article dans la liste des articles
+        for (Article article : articles) {
+            // Affichage de l'article
+            System.out.println(article.toString());
+        }
+    }
+
+    // La méthode pour afficher le total de la commande
+    public void afficherTotal() {
+        // Affichage du total de la commande
+        System.out.println("Total : " + total);
+    }
 }
--- projet/Exercice 7/src/Main.java
+++ projet/Exercice 7/src/Main.java
@@ -5,7 +5,18 @@ public class Main {
         Client client = new Client("Dupont", "Jean", "1 rue de la Paix", "75008", "Paris");
         System.out.println(client + "\n");
         Article pc = new Article("ref123", "Ordinateur portable", 999.99);
+        Article manette = new Article("ref111", "manette", 99.90);
         System.out.println(pc);
 
+        // Créer un objet de la classe Commande
+        Commande commande = new Commande();
+
+        // Ajouter les articles à la commande
+        commande.ajouterArticle(pc);
+        commande.ajouterArticle(manette);
+
+
+        // Afficher le total de la commande
+        System.out.println("Total de la commande : " + commande.calculerTotal());
     }
 }
