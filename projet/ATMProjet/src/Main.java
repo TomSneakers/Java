619d7983032b530a606a4d466092f5c6a1463341
Tom <tomdesvignes031@gmail.com>
2023-07-12 15:06:09 +0200

    projetATM

--- projet/ATMProjet/src/Main.java
+++ projet/ATMProjet/src/Main.java
@@ -1,31 +1,8 @@
-import java.util.Scanner;
-
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        Client client = new Client("Dupont", "Jean", 4545, 500);
-        System.out.println(client);
 
-
-        System.out.print("Veuillez entrer votre code pin:");
-        Scanner scPin = new Scanner(System.in);
-        int pin = scPin.nextInt();
-
-        while (true) {
-            if (client.getPin() == pin) {
-                System.out.println("Le code entré est correct.");
-                System.out.println("Veuillez choisir : ");
-                System.out.println("1. Consulter le solde");
-                System.out.println("2. Déposer de l'argent");
-                System.out.println("3. Retirer de l'argent");
-                System.out.println("4. Sortir du distributeur");
-                System.out.print("Faite votre choix : ");
-                break;
-
-            } else {
-                System.out.println("Le code n'est pas le bon.");
-                System.out.print("Veuillez resaisir votre code pin:");
-                pin = scPin.nextInt();
-            }
         }
     }
 }
