e25eb5ddf0b975676285c51a59ad51f01d38d50d
Tom <tomdesvignes031@gmail.com>
2023-07-12 12:24:49 +0200

    ATM

--- projet/ATMProjet/src/Client.java
+++ projet/ATMProjet/src/Client.java
@@ -5,24 +5,23 @@ public class Client {
     // Les attributs de la classe Client
     private String nom;
     private String prenom;
-    private String adresse;
-    private String codePostal;
-    private String ville;
-    private LocalDate dateNaissance;
+    private int pin;
+
+    private int solde;
 
     //Le constructeur de l'age du client:
     //public int getAge(){
-      //  return dateNaissance.until(LocalDate.now()).getYears();}
+    //  return dateNaissance.until(LocalDate.now()).getYears();}
 
 
     // Le constructeur de la classe Client
-    public Client(String nom, String prenom, String adresse, String codePostal, String ville) {
+    public Client(String nom, String prenom, int pin, int solde) {
         // Initialisation des attributs de la classe avec les paramètres du constructeur
         this.nom = nom;
         this.prenom = prenom;
-        this.adresse = adresse;
-        this.codePostal = codePostal;
-        this.ville = ville;
+        this.pin = pin;
+        this.solde = solde;
+
 
     }
 
@@ -34,27 +33,15 @@ public class Client {
     public String getPrenom() {
         return prenom;
     }
-
-    public String getAdresse() {
-        return adresse;
-    }
-
-    public String getCodePostal() {
-        return codePostal;
-    }
-
-    public String getVille() {
-        return ville;
+    public int getPin(){
+        return pin;
     }
 
     // La méthode toString pour afficher une représentation textuelle de l'objet Client
     public String toString() {
         return "Client{" +
                 "nom='" + nom + '\'' +
-                ", prenom='" + prenom + '\'' +
-                ", adresse='" + adresse + '\'' +
-                ", codePostal='" + codePostal + '\'' +
-                ", ville='" + ville + '\'' +
+                ", prenom='" + prenom + '\'' + ",solde=" + solde + "€" +
                 '}';
     }
 }
