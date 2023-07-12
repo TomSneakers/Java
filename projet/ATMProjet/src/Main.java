0b5ae0b74b0ba68310545146f1c2bdcffe287edc
Tom <tomdesvignes031@gmail.com>
2023-07-12 15:52:29 +0200

    main and bank

--- projet/ATMProjet/src/Main.java
+++ projet/ATMProjet/src/Main.java
@@ -10,7 +10,32 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         //Boucle de la méthode main qui permet de ce dépllacer et ne pas recommencer les opération
+        while (true) {
+            //Identification
+            System.out.print("Veuillez entrer votr code PIN : ");
 
+            // Lecture du code PIN saisi par l'utilisateur
+            int pin = scanner.nextInt();
+
+            if (atm.authenticate(pin)) {
+                // Affiche le menue grace à la ckasse ATM
+                atm.displayMenu();
+
+
+                // Lecture du choix de l'utilisateur
+                int choice = scanner.nextInt();
+
+                // Appel de la méthode performOperation de la classe ATM pour effectuer l'opération choisie
+                atm.performOperation(choice);
+            } else {
+                System.out.println("Authentification échouée. Veuillez réessayer.");
+            }
+            System.out.print("Voulez-vous effectuer une autre opération ? yes or non ");
+            // Lecture de l'option de l'utilisateur pour continuer ou quitter
+            String option = scanner.next();
+            if (!option.equalsIgnoreCase("yes")) {
+                break;
+            }
         }
     }
-
+}
