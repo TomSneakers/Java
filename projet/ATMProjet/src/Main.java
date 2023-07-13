40fc5d9e66110cf56b43ad3fd6bc79d591a8b9ae
Tom <tomdesvignes031@gmail.com>
2023-07-13 13:50:05 +0200

    pour exam

--- projet/ATMProjet/src/Main.java
+++ projet/ATMProjet/src/Main.java
@@ -5,34 +5,54 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String dbPath = "./atm.db";
+        // Crée une nouvelle instance de la classe Bank
         Bank bank = new Bank();
+
+        // Crée une nouvelle instance de la classe ATM en lui passant l'objet bank en paramètre
         ATM atm = new ATM(bank);
+
+        // Connecte à la base de données en utilisant la méthode connect de la classe Sqlite
         Sqlite.getInstance().connect();
 
+        // Crée une instance de la classe Scanner pour lire l'entrée utilisateur
         Scanner scanner = new Scanner(System.in);
 
-        while (true) {
+        while (true) {  // Boucle principale
+
+            // Affiche le message demandant au utilisateur d'entrer son code PIN
             System.out.print("Veuillez entrer votre code PIN : ");
+
+            // Lit l'entier entré et le stocke dans la variable pin
             int pin = scanner.nextInt();
 
+            // Vérifie si le code PIN est valide en appelant la méthode authenticate de l'objet atm
             if (atm.authenticate(pin)) {
+
+                // Si la methode qui verifie le code pin retourne true alors il affiche le menue de l'objet atm
                 atm.displayMenu();
 
+                // Lit l'entier entré par l'utilisateur et le stocke dans la variable choice
                 int choice = scanner.nextInt();
+
+                // Exécute l'opération choisie par l'utilisateur en appelant la méthode performOperation de l'objet atm
                 atm.performOperation(choice);
             } else {
+                // si l'authent echoue
                 System.out.println("Authentification échouée. Veuillez réessayer.");
             }
+
 
             System.out.print("Voulez-vous effectuer une autre opération ? yes or non ");
             String option = scanner.next();
 
+            // Vérifie si option est l'entrer est egal a yes
             if (!option.equalsIgnoreCase("yes")) {
+
+                // Sort de la boucle principale si l'option n'est pas "yes"
                 break;
             }
         }
-
+        // Ferme la connexion à la base de données en appelant la méthode close de la classe Sqlite
         Sqlite.getInstance().close();
     }
 }
--- projet/ATMProjet/src/database/Sqlite.java
+++ projet/ATMProjet/src/database/Sqlite.java
@@ -1,48 +1,70 @@
 package database;
-
 import java.sql.*;
 import java.util.ArrayList;
 
 public class Sqlite {
+
+    // Chemin du fichier de base de données
     private String dbPath;
+
+    // Objet Connection pour établir la connexion à la base de données
     private Connection connection = null;
 
+    // Instance unique de la classe Sqlite
     private static Sqlite instance = null;
 
     public static Sqlite getInstance() {
         if (instance == null)
+            // Crée une nouvelle instance de Sqlite avec le chemin du fichier de base de données
             instance = new Sqlite("atm.db");
 
         return instance;
     }
 
+    //Constructeur de Sqlite qui prend en parametre une chaine de caractère la variable dbPath (donc le chemin de la base de donnée)
     public Sqlite(String dbPath) {
+
+        // Initialise le chemin du fichier de base de données avec la valeur fournie en paramètre
         this.dbPath = dbPath;
     }
 
     public void connect() {
         try {
+            // Charge le pilote JDBC pour SQLite (le fameux mdr)
             Class.forName("org.sqlite.JDBC");
+
+            // Établit la connexion à la base de données SQLite en utilisant le chemin du fichier
             connection = DriverManager.getConnection("jdbc:sqlite:" + dbPath);
+
+            //En cas d'exception ces bloque permette d'afficher les erreurs
         } catch (ClassNotFoundException notFoundException) {
             notFoundException.printStackTrace();
+
+            // Affiche un message d'erreur en cas d'échec de chargement du pilote JDBC
             System.out.println("Erreur de connexion");
         } catch (SQLException sqlException) {
             sqlException.printStackTrace();
+            // Affiche un message d'erreur en cas d'échec de connexion à la base de données
             System.out.println("Erreur de connexion");
         }
     }
 
+    //Constructeur de la methode ExecuteQueryRow
+    //Nino ma aider pour cette partie ;)
     public Object[] ExecuteQueryRow(String sql, Object... args) {
         try {
+            // Prépare la requête SQL en utilisant l'objet Connection
             var statement = connection.prepareStatement(sql);
             var index = 1;
             for (var arg : args) {
+                // Remplit les paramètres de la requête SQL avec les valeurs fournies en paramètre
                 statement.setObject(index, arg);
                 index++;
             }
+            // Exécute la requête SQL et récupère le résultat dans un objet ResultSet
             var res = statement.executeQuery();
 
+            // Extrait les données du résultat et retourne la première ligne sous forme de tableau d'objets
             return ExtractData(res).iterator().next();
         } catch (Exception e) {
             System.out.println(e);
@@ -53,12 +75,15 @@ public class Sqlite {
 
     public void ExecuteNonQuery(String sql, Object... args) {
         try {
+            // Prépare la requête SQL en utilisant l'objet Connection
             var statement = connection.prepareStatement(sql);
             var index = 1;
             for (var arg : args) {
+                // Remplit les paramètres de la requête SQL avec les valeurs fournies en paramètre
                 statement.setObject(index, arg);
                 index++;
             }
+            // Exécute la requête SQL qui ne retourne pas de résultat
             statement.execute();
         } catch (Exception e) {
             System.out.println(e);
@@ -66,22 +91,25 @@ public class Sqlite {
     }
 
     private Iterable<Object[]> ExtractData(ResultSet res) throws SQLException {
+
+        // Crée une nouvelle instance d'ArrayList (tableau d'objet) et plusieurs dimansion pour stocker les données extraites
         var result = new ArrayList<Object[]>();
+        // Parcourt chaque ligne du résultat
         while (res.next()) {
-            var columnNumber = res.getMetaData().getColumnCount();
-            var row = new Object[columnNumber];
+            var columnNumber = res.getMetaData().getColumnCount();  // Récupère le nombre de colonnes du résultat
+            var row = new Object[columnNumber];  // Crée un tableau d'objets pour stocker les valeurs de la ligne courante
             for (int i = 0; i <= columnNumber - 1; i++) {
-                row[i] = res.getObject(i + 1);
+                row[i] = res.getObject(i + 1);  // Récupère la valeur de chaque colonne de la ligne courante et l'ajoute au tableau
             }
-            result.add(row);
+            result.add(row);  // Ajoute le tableau représentant la ligne dans l'ArrayList
         }
 
-        return result;
+        return result;  // Retourne l'ArrayList contenant les données extraites
     }
 
     public void close() {
         try {
-            connection.close();
+            connection.close();  // Ferme la connexion à la base de données
             System.out.println("Connexion à la base de données fermée");
         } catch (SQLException e) {
             e.printStackTrace();
